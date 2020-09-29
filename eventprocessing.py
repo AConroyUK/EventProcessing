ac88238bc7647e53a268d400b24408cb73a234ba
CONROY Aedan <aconroy@AltranUK.local>
2020-09-29 16:14:59 +0100

    Recieving messages

--- eventprocessing.py
+++ eventprocessing.py
@@ -5,28 +5,94 @@ import boto3
 
 class eventProcessing:
 
-    def __init__(self):
-        self.ACCESS_KEY = ""
-        self.SECRET_KEY = ""
-        self.BUCKET_NAME = "eventprocessing-altran-locationss3bucket-1ub1fsm0jlky7"
-
     def main(self):
         log = logHandler()
         file = fileHandler(log)
         log.info("Started")
-
-        self.ACCESS_KEY, self.SECRET_KEY = file.loadkeys()
+        config = file.loadconfig()
+        access_key = config["ACCESS_KEY"]
+        secret_key = config["SECRET_KEY"]
+        bucket_name = config["BUCKET_NAME"]
+        bucket_key = config["BUCKET_KEY"]
+        topic_arn = config["SNS_TOPIC_ARN"]
 
         s3 = boto3.client(
              's3',
-             aws_access_key_id=self.ACCESS_KEY,
-             aws_secret_access_key=self.SECRET_KEY,
-             config=file.loadconfig()
+             aws_access_key_id=access_key,
+             aws_secret_access_key=secret_key,
+             config=config["AWS_CONFIG"]
+        )
+        sqs = boto3.client(
+             'sqs',
+             aws_access_key_id=access_key,
+             aws_secret_access_key=secret_key,
+             config=config["AWS_CONFIG"]
+        )
+        sns = boto3.client(
+             'sns',
+             aws_access_key_id=access_key,
+             aws_secret_access_key=secret_key,
+             config=config["AWS_CONFIG"]
         )
 
-        s3.download_file(self.BUCKET_NAME, 'locations.json', 'locations.json')
+        s3.download_file(bucket_name, bucket_key, 'locations.json')
+        locations = file.loadjson('locations.json')
 
-        file.loadjson('locations.json')
+        response = sqs.create_queue(QueueName='sensors')
+        log.info("Queue created")
+        queue_url = response["QueueUrl"]
+        response = sqs.get_queue_attributes(
+            QueueUrl=queue_url,
+            AttributeNames=['QueueArn']
+        )
+        queue_arn = response["Attributes"]["QueueArn"]
+        log.debug("Queue url: "+queue_url)
+        log.debug("Queue ARN: "+queue_arn)
+
+        policy_document = {
+            'Version': '2012-10-17',
+            'Statement': [{
+                'Sid': f'allow-subscription-{topic_arn}',
+                'Effect': 'Allow',
+                'Principal': {'AWS': '*'},
+                'Action': 'SQS:SendMessage',
+                'Resource': f'{queue_arn}',
+                'Condition': {
+                    'ArnEquals': { 'aws:SourceArn': f'{topic_arn}' }
+                }
+            }]
+        }
+        policy_json = json.dumps(policy_document)
+
+        response = sqs.set_queue_attributes(
+            QueueUrl=queue_url,
+            Attributes={
+                "Policy": policy_json
+            }
+        )
+
+        response = sns.subscribe(
+            TopicArn=topic_arn,
+            Protocol='sqs',
+            Endpoint=queue_arn
+        )
+        subscriptionArn = response["SubscriptionArn"]
+        log.debug("Subscription ARN: "+subscriptionArn)
+
+        response = sqs.receive_message(
+            QueueUrl=queue_url,
+            MaxNumberOfMessages=10,
+            VisibilityTimeout=20,
+            WaitTimeSeconds=20
+        )
+        for message in response["Messages"]:
+            log.debug(message)
+
+
+
+
+        response = sqs.delete_queue(QueueUrl=queue_url)
+        log.info("Queue deleted")
         log.info("Ended")
 
 
--- fileHandler.py
+++ fileHandler.py
@@ -7,30 +7,21 @@ class fileHandler:
         global log
         log = logHandler
 
-    def loadkeys(self):
-          with open('aws.config') as configfile:
-              configData = json.load(configfile)
-              log.info("AWS keys loaded")
-              return configData['ACCESS_KEY'], configData['SECRET_KEY']
-
     def loadconfig(self):
-        log.info("Config loaded")
-        return Config(region_name = 'eu-west-1')
-            # signature_version = 'v4',
-            # retries = {
-            #     'max_attempts': 5,
-            #     'mode': 'standard'
-            # })
+        with open('eventProcessing.config') as configfile:
+            configData = json.load(configfile)
+            configData["AWS_CONFIG"] = Config(region_name = 'eu-west-1')
+            # Config(region_name = 'eu-west-1')
+            #     signature_version = 'v4',
+            #     retries = {
+            #         'max_attempts': 5,
+            #         'mode': 'standard'
+            #     })
+            log.info("Config loaded")
+            return configData
 
     def loadjson(self,path):
         with open(path) as jsonfile:
             data = json.load(jsonfile)
-            for dict in data:
-                x = str(dict["x"])
-                y = str(dict["y"])
-                id = str(dict["id"])
-        log.info(path + " loaded")
-
-        #         self.tHandler.readRow([date,fromAccount,toAccount,narrative,amount])
-        # logging.info("Imported file:" + str(path))
-        # self.tHandler.transactions.sort(key=self.sortKey)
+            log.info(path + " loaded")
+            return data
