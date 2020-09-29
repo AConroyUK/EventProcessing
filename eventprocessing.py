9fdd0e0d6d10046c81c5fba1e1f15283fed102f8
CONROY Aedan <aconroy@AltranUK.local>
2020-09-29 11:39:24 +0100

    get bucket

--- eventprocessing.py
+++ eventprocessing.py
@@ -1,23 +1,31 @@
 import json
-import fileHandler
+from fileHandler import fileHandler
 import boto3
 
 class eventProcessing:
 
     def __init__(self):
-        self.ACCESS_KEY, self.SECRET_KEY, self.SESSION_TOKEN = fileHandler.loadkeys()
+        self.ACCESS_KEY = ""
+        self.SECRET_KEY = ""
+        self.BUCKET_NAME = "eventprocessing-altran-locationss3bucket-1ub1fsm0jlky7"
 
     def main(self):
+        fHandler = fileHandler()
+        self.ACCESS_KEY, self.SECRET_KEY = fHandler.loadkeys()
 
-        client = boto3.client(
-            's3',
-            aws_access_key_id=self.ACCESS_KEY,
-            aws_secret_access_key=self.SECRET_KEY,
-            aws_session_token=self.SESSION_TOKEN,
-            config=fileHandler.loadconfig()
+        s3 = boto3.resource(
+             's3',
+             aws_access_key_id=self.ACCESS_KEY,
+             aws_secret_access_key=self.SECRET_KEY,
+             config=fHandler.loadconfig()
         )
 
+        bucket = s3.Bucket(name=self.BUCKET_NAME)
 
-        s3 = boto3.resource('s3')
-        for bucket in s3.buckets.all():
-            print(bucket.name)
+        print(bucket.name)
+
+
+
+
+if __name__ == "__main__":
+    eventProcessing().main()
